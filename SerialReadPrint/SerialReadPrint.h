8ceb4f903e34dd79193e19bd782dc55c9b308c1d
JohnLim <johnlimjsj@gmail.com>
2016-07-11 21:24:01 -0700

    all of my Arduino Libraries

---
+++ SerialReadPrint/SerialReadPrint.h
@@ -0,0 +1,27 @@
+#include "Arduino.h"
+
+class SerialReadPrint
+{
+  public:
+    void s_print(String message, int result);
+    void s_print(String message, float result);
+    void s_print(String message, char result);
+    void s_print(String message, String result);
+    
+    void s_println(String message, int result);
+    void s_println(String message, float result);
+    void s_println(String message, char result);
+    void s_println(String message, String result);   
+
+ /* ==== To read from Serial buffer ======== */
+    void s_read(String, int *);
+    void s_read(String, float *);
+    void s_read(String, char *);
+    void s_read(String, String *);
+
+
+  private:
+    void _read_message(String);
+    void _read_clearbuff();
+};
+
